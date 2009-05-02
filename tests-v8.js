354eb429c3b6f2e6bb057695a312b2dfc3ac01b6
Sergey Berezhnoy <veged@mail.ru>
2009-05-03 03:12:35 +0400

    Правильные вложенные инлайновые элементы + тесты для запуска из v8.

---
+++ tests-v8.js
@@ -0,0 +1,46 @@
+var tests = [
+    {
+        'in': '**bold******',
+        'out': '[[para, [[bold, [bold]], [bold, []]]]]'
+    },
+    {
+        'in': '//italic//',
+        'out': '[[para, [[italic, [italic]]]]]'
+    },
+    {
+        'in': '--strike--',
+        'out': '[[para, [[strike, [strike]]]]]'
+    },
+    {
+        'in': '**bb//bb**nn**bb//bb**',
+        'out': '[[para, [[bold, [bb, [italic_, [bb]]]], nn, [bold, [bb, [italic_, [bb]]]]]]]'
+    },
+    {
+        'in': '~**esc bold**',
+        'out': '[[para, [[escaped, *], *esc bold, [bold_, []]]]]'
+    },
+    {
+        'in': '**//bolditalic//**',
+        'out': '[[para, [[bold, [[italic, [bolditalic]]]]]]]'
+    },
+    {
+        'in': 'para1**bla**\n//dsds//\n\npara2\npara2\n\npara3',
+        'out': '[[para, [para1, [bold, [bla]], \n, [italic, [dsds]]]], [para, [para2\npara2]], [para, [para3]]]'
+    }
+];
+
+load("ometa-rhino.js");
+load('shmakowiki.txt');
+
+for (var i = 0; i < tests.length; i++) {
+    var test = tests[i];
+
+    var testRes = W.matchAll(test['in'], 'topLevel'),
+        isOk = testRes == test.out;
+
+    print('Test in "' + test['in'] + '": ' + (isOk ? 'ok' : 'FAIL'));
+    if (!isOk) {
+        print('Test result:\n' + testRes);
+        print('Test out:\n' + test.out);
+    }
+}
