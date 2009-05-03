47723001049e86cfcedad864eb1cfb8b67f7e1e3
Sergey Berezhnoy <veged@mail.ru>
2009-05-04 02:28:14 +0400

    Вложенные простые списки.

--- tests-v8.js
+++ tests-v8.js
@@ -28,16 +28,24 @@ var tests = [
         'out': '[[para, [para1, [bold, [bla]], \n, [italic, [dsds]]]], [para, [para2\npara2]], [para, [para3]]]'
     },
     {
-        'in': '* listitem1\n* listitem2',
-        'out': '[[list, [[listItem, [listitem1]], [listItem, [listitem2]]]]]'
+        'in': '* listitem1\n* listitem2\n  * sublistitem2',
+        'out': '[[list, [[listItem, [listitem1]], [listItem, [listitem2, [list, [[listItem, [sublistitem2]]]]]]]]]'
+    },
+    {
+        'in': '* listitem1\n  * sublistitem1\n    * subsublistitem1',
+        'out': '[[list, [[listItem, [listitem1, [list, [[listItem, [sublistitem1, [list, [[listItem, [subsublistitem1]]]]]]]]]]]]]'
+    },
+    {
+        'in': '* listitem1\n* listitem2\n  * sublistitem21\n* listitem3\n  * sublistitem31\n  * sublistitem32',
+        'out': '[[list, [[listItem, [listitem1]], [listItem, [listitem2, [list, [[listItem, [sublistitem21]]]]]], [listItem, [listitem3, [list, [[listItem, [sublistitem31]], [listItem, [sublistitem32]]]]]]]]]'
     },
     {
         'in': 'para1\n* listitem1\n* listitem2\n\npara2',
         'out': '[[para, [para1]], [list, [[listItem, [listitem1]], [listItem, [listitem2]]]], [para, [para2]]]'
     },
     {
-        'in': '* list **bold**\n* list //italic//\n* list **bo//italic_**',
-        'out': '[[list, [[listItem, [list , [bold, [bold]]]], [listItem, [list , [italic, [italic]]]], [listItem, [list , [bold, [bo, [italic_, [italic_]]]]]]]]]'
+        'in': '* listitem **bold**\n* listitem //italic//\n* listitem **bo//italic_**',
+        'out': '[[list, [[listItem, [listitem , [bold, [bold]]]], [listItem, [listitem , [italic, [italic]]]], [listItem, [listitem , [bold, [bo, [italic_, [italic_]]]]]]]]]'
     }
 ];
 
@@ -50,9 +58,10 @@ for (var i = 0; i < tests.length; i++) {
     var testRes = W.matchAll(test['in'], 'topLevel'),
         isOk = testRes == test.out;
 
-    print('Test in "' + test['in'] + '": ' + (isOk ? 'ok' : 'FAIL'));
+    print('Test in:\n' + test['in'] + '\n: ' + (isOk ? 'ok' : 'FAIL'));
     if (!isOk) {
         print('Test result:\n' + testRes);
         print('Test out:\n' + test.out);
     }
+    print('-----------------------------------------------------');
 }
