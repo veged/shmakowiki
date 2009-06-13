7b856f6aa8278419f3d6597446c18844312212d3
Sergey veged Berezhnoy <veged@mail.ru>
2009-06-13 18:06:29 +0400

    Скрипт для преобразования в html из консоли.

---
+++ wiki2html.js
@@ -0,0 +1,11 @@
+load("ometa-rhino.js");
+load('shmakowiki.txt');
+load('shmakowiki2html.txt');
+
+print(ShmakoWikiToHtml.match(
+    ShmakoWiki.matchAll(
+        read('shmakowiki.wiki').slice(1),
+        'topLevel'
+    ),
+    'topLevel'
+));
